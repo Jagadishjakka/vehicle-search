09a205999bb63b61f5d16cef3d5fe12df44f8b5c
jagadish jakka <jagadish.jakka@gmail.com>
2018-06-25 11:41:34 -0500

    'initialsetup'

---
+++ src/main/java/com/jakka/vehiclesearch/VehicleSearchApplication.java
@@ -0,0 +1,12 @@
+package com.jakka.vehiclesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleSearchApplication.class, args);
+	}
+}
